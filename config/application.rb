cee97c6d3ab40bd8dbb88e55e8a98659a2d1742d
Jennie Louie <jennie.louie@gmail.com>
2014-01-11 19:13:40 -0800

    revised application.rb precompile

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Mango
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Added the line below for Storyteller
+    config.assets.initialize_on_precompile = false
   end
 end
